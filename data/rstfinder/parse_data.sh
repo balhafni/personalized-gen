7e6f1d1c8479b94587b98bb2326884f675445bb1
balhafni <ba63@nyu.edu>
2024-02-06 06:41:41 +0000

    RST pare script

--- data/rstfinder/parse_data.sh
+++ data/rstfinder/parse_data.sh
@@ -1,12 +1,5 @@
-split=train
-dir=../data/rst_data/${split}_shards
-for s in $dir/*
-do
-	echo "Parsing files in $s..."
-	input_files=$(for k in $s/*txt; do echo $k; done)
-	rst_parse -g segmentation_model.best -p  rst_parsing_model.best  $input_files > $s/${split}_parsed.json
-	# paste $dir/${split}_files ../data/rst_data/${split}_parsed.json > ../data/rst_data/${split}_parsed_files.json
-done
+dir=../data/rst_data
 
+input_files=$(for k in $dir/*; do echo $k; done)
 
-
+rst_parse -g segmentation_model.best -p  rst_parsing_model.best  $input_files > $dir/rst_parsed.json
